258de902c0805870b1b643b7dd66a5792d6d8fb8
Azuxul <Azuxul@gmail.com>
2015-03-13 19:11:03 +0100

    Fix issue #1

--- java/azuxul/morelight/blocks/PhosphoreBlock.java
+++ java/azuxul/morelight/blocks/PhosphoreBlock.java
@@ -1,6 +1,7 @@
 package azuxul.morelight.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.util.EnumWorldBlockLayer;
@@ -12,14 +13,20 @@ public class PhosphoreBlock extends Block {
 
 	public PhosphoreBlock(){
 		
-		super(Material.ground);
+		super(new Material(MapColor.goldColor));
 		this.setUnlocalizedName("phosphoreblock");
 		this.setCreativeTab(CreativeTabs.tabBlock);
 		this.setLightLevel(1.0F);
 		this.setHardness(0.15F);
+		this.setResistance(1.0F);
 
 	}
 	
+	public boolean isOpaqueCube() {
+		
+		return false;
+	}
+
 	@SideOnly(Side.CLIENT)
 	public EnumWorldBlockLayer getBlockLayer() {
 		return EnumWorldBlockLayer.TRANSLUCENT;
